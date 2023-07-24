404085ede0e3e08f1dc4cd759e9352109b76b96b
Christopher Serr <christopher.serr@gmail.com>
2023-07-24 21:34:55 +0200

    Use upstream Unity support
    The Unity support is now upstreamed into the `asr` crate, so we can
    start using it here and throw out our own implementation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,73 +1,67 @@
 #![no_std]
-#![allow(non_snake_case)] // TODO: Fix
 #![cfg_attr(
     feature = "nightly",
     feature(type_alias_impl_trait, const_async_blocks)
 )]
 
 use arrayvec::ArrayString;
-use asr_dotnet::{
-    asr::{
-        self,
-        future::{next_tick, retry},
-        print_message,
-        time::Duration,
-        timer::{self, TimerState},
-        watcher::Watcher,
-        Process,
-    },
-    MonoClass, MonoClassBinding, MonoImage, MonoModule, Ptr,
+use asr::{
+    future::next_tick,
+    game_engine::unity::il2cpp::{Class, Module, Version},
+    print_message,
+    time::Duration,
+    timer::{self, TimerState},
+    watcher::Watcher,
+    Address, Process,
 };
 use bytemuck::{Pod, Zeroable};
 
 asr::panic_handler!();
 
 struct GameInfo {
-    timer_instance: Ptr,
-    game_manager_instance: Ptr,
-    timer_binding: TimerBinding,
-    game_manager_binding: GameManagerBinding,
+    timer_instance: Address,
+    game_manager_instance: Address,
+    timer_class: TimerBinding,
+    game_manager_class: GameManagerBinding,
 }
 
 impl GameInfo {
     async fn load(process: &Process) -> Self {
-        let mono_module = MonoModule::locate(process).await;
+        let module = Module::wait_attach(process, Version::V2020).await;
 
         print_message("Found Mono");
 
-        let image = retry(|| mono_module.find_image(process, "Assembly-CSharp")).await;
+        let image = module.wait_get_default_image(process).await;
 
         print_message("Found Assembly-CSharp");
 
-        let timer_binding = retry(|| Timer::bind(&image, process, &mono_module)).await;
-
-        let timer_instance =
-            retry(|| timer_binding.class.find_singleton(process, "_instance")).await;
+        let timer_class = Timer::bind(process, &module, &image).await;
+        let timer_instance = timer_class
+            .class()
+            .wait_get_static_instance(process, &module, "_instance")
+            .await;
 
         print_message("Found Timer");
 
-        let game_manager_binding = retry(|| GameManager::bind(&image, process, &mono_module)).await;
-
-        let game_manager_instance = retry(|| {
-            game_manager_binding
-                .class
-                .find_singleton(process, "<Instance>k__BackingField")
-        })
-        .await;
+        let game_manager_class = GameManager::bind(process, &module, &image).await;
+        let game_manager_instance = game_manager_class
+            .class()
+            .wait_get_static_instance(process, &module, "<Instance>k__BackingField")
+            .await;
 
         print_message("Found GameManager");
 
         Self {
             timer_instance,
             game_manager_instance,
-            timer_binding,
-            game_manager_binding,
+            timer_class,
+            game_manager_class,
         }
     }
 }
 
 #[repr(C)]
-#[derive(Default, Copy, Clone, Pod, Zeroable, Debug)]
+#[derive(Copy, Clone, Pod, Zeroable)]
 struct Digits {
     minutes: f32,
     seconds: f32,
@@ -95,13 +89,13 @@ impl Digits {
 
 impl GameManager {
     fn stage(&self) -> i32 {
-        ((self.currentLevel / 2) + 1).min(7)
+        ((self.level / 2) + 1).min(7)
     }
 
     fn act(&self) -> char {
-        if self.currentLevel == LEVEL_7_X {
+        if self.level == LEVEL_7_X {
             'X'
-        } else if self.currentLevel & 1 == 0 {
+        } else if self.level & 1 == 0 {
             '1'
         } else {
             '2'
@@ -116,19 +110,26 @@ impl GameManager {
     }
 }
 
-#[derive(Copy, Clone, Default, MonoClassBinding)]
+#[derive(Copy, Clone, Class)]
 struct GameManager {
-    gameState: i32,
-    _points: i32,
-    _deaths: i32,
-    currentLevel: i32,
+    #[rename = "gameState"]
+    game_state: i32,
+    #[rename = "_points"]
+    points: i32,
+    #[rename = "_deaths"]
+    deaths: i32,
+    #[rename = "currentLevel"]
+    level: i32,
 }
 
-#[derive(Copy, Clone, Default, MonoClassBinding)]
+#[derive(Copy, Clone, Class)]
 struct Timer {
-    currentLevelTime: f32,
-    currentLevelTimeVector: Digits,
-    timerStopped: u8,
+    #[rename = "currentLevelTime"]
+    level_time: f32,
+    #[rename = "currentLevelTimeVector"]
+    level_time_vector: Digits,
+    #[rename = "timerStopped"]
+    timer_stopped: bool,
     character: u32,
 }
 
@@ -170,6 +171,8 @@ async fn main() {
     let mut beyond_first_level = false;
 
     loop {
+        asr::set_tick_rate(1.0);
+
         let process = Process::wait_attach("Lunistice.exe").await;
         process
             .until_closes(async {
@@ -179,28 +182,30 @@ async fn main() {
                 let mut game_manager = Watcher::new();
                 let mut timer_state = Watcher::new();
 
+                asr::set_tick_rate(120.0);
+
                 loop {
                     let game_manager = game_manager.update(
                         game_info
-                            .game_manager_binding
-                            .load(&process, game_info.game_manager_instance)
+                            .game_manager_class
+                            .read(&process, game_info.game_manager_instance)
                             .ok(),
                     );
 
                     let timer = timer.update(
                         game_info
-                            .timer_binding
-                            .load(&process, game_info.timer_instance)
+                            .timer_class
+                            .read(&process, game_info.timer_instance)
                             .ok(),
                     );
 
                     if let (Some(game_manager), Some(timer)) = (game_manager, timer) {
                         let mut buffer = itoa::Buffer::new();
-                        timer::set_variable("Points", buffer.format(game_manager._points));
-                        timer::set_variable("Resets", buffer.format(game_manager._deaths));
+                        timer::set_variable("Points", buffer.format(game_manager.points));
+                        timer::set_variable("Resets", buffer.format(game_manager.deaths));
 
                         let mut string_buffer = ArrayString::<32>::new();
-                        timer.currentLevelTimeVector.format_into(&mut string_buffer);
+                        timer.level_time_vector.format_into(&mut string_buffer);
                         timer::set_variable("Level Time", &string_buffer);
                         string_buffer.clear();
                         game_manager.format_level_into(&mut string_buffer);
@@ -220,31 +225,29 @@ async fn main() {
 
                         match timer_state.current {
                             TimerState::NotRunning => {
-                                if timer.check(|t| t.timerStopped == 0)
-                                    && game_manager.currentLevel == LEVEL_1_1
+                                if timer.check(|t| !t.timer_stopped)
+                                    && game_manager.level == LEVEL_1_1
                                 {
                                     timer::start();
                                 }
                             }
                             TimerState::Paused | TimerState::Running => {
-                                if timer.current.currentLevelTime < timer.old.currentLevelTime {
+                                if timer.current.level_time < timer.old.level_time {
                                     if !beyond_first_level {
                                         timer::reset();
                                         return;
                                     }
-                                    run_time += Duration::saturating_seconds_f32(
-                                        timer.old.currentLevelTime,
-                                    );
+                                    run_time +=
+                                        Duration::saturating_seconds_f32(timer.old.level_time);
                                 }
 
                                 timer::set_game_time(
-                                    run_time
-                                        + Duration::saturating_seconds_f32(timer.currentLevelTime),
+                                    run_time + Duration::saturating_seconds_f32(timer.level_time),
                                 );
 
-                                if game_manager.check(|g| g.gameState == game_state::RESULTS)
-                                    || (game_manager.old.currentLevel >= LEVEL_7_2
-                                        && game_manager.current.currentLevel == LEVEL_2_1)
+                                if game_manager.check(|g| g.game_state == game_state::RESULTS)
+                                    || (game_manager.old.level >= LEVEL_7_2
+                                        && game_manager.current.level == LEVEL_2_1)
                                 {
                                     beyond_first_level = true;
                                     timer::split();
